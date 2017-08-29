8ce5199ab62e036f0b5d99db87e7c16968b66317
zhongyiares <zhongyiares@qq.com>
2017-08-29 17:01:46 +0800

    test

---
+++ src/test/java/com/thinkgem/jeesite/test/ZYTest.java
@@ -0,0 +1,11 @@
+package com.thinkgem.jeesite.test;
+
+/**
+ * Created by zhongyi on 2017/8/29 0029.
+ */
+public class ZYTest {
+
+    public static void main(String[] args) {
+        System.out.println("ok...");
+    }
+}
