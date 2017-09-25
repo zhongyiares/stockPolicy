192409c8c0210a6129618d516dbc54dbe07b021b
zhongyiares <zhongyiares@qq.com>
2017-09-25 17:07:29 +0800

    add stockPolicy

--- src/main/java/com/thinkgem/jeesite/modules/stock/util/MongoDBTest.java
+++ src/main/java/com/thinkgem/jeesite/modules/stock/util/MongoDBTest.java
@@ -10,10 +10,18 @@ import com.mongodb.util.JSON;
  */
 public class MongoDBTest {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient("192.168.199.240",27017);
+        MongoClientOptions msc = MongoClientOptions.builder().connectionsPerHost(6).connectTimeout(1000).build();
 
+        //MongoClient mongoClient = new MongoClient("192.168.199.240",27017);
+
+        MongoClient mongoClient = new MongoClient("192.168.2.241:27017",msc);
+
+        System.out.println(mongoClient.getMongoClientOptions().getConnectionsPerHost());
         DB db = mongoClient.getDB("mydbs");
         DBCollection users =  db.getCollection("users");
+
+
+
         // method 1
 //        BasicDBObject d1 = new BasicDBObject();
 //        d1.append("userId","2").append("name","zy2");
@@ -25,8 +33,8 @@ public class MongoDBTest {
 //        users.insert(builder.get());
 
         //method3
-        String json = "{'userId':'3','sex':'man2'}";
-        DBObject d1 = (DBObject) JSON.parse(json);
+//        String json = "{'userId':'2','sex':'man2'}";
+//        DBObject d1 = (DBObject) JSON.parse(json);
 //        users.insert(d1);
 
         //remove
@@ -34,13 +42,23 @@ public class MongoDBTest {
 //        users.remove(new BasicDBObject());
 
         //upate
-        users.update(new BasicDBObject("userId","3"),d1);
+        //users.update(new BasicDBObject("userId","3"),d1);
+//        users.update(new BasicDBObject("userId","3"),new BasicDBObject("$set",new BasicDBObject("name","newName")));
+
+
+        //find
+//        DBObject d = users.findOne();
+//        System.out.println(d);
+
+//        DBObject con = new BasicDBObject("userId","3");
+//        DBCursor c = users.find(con).limit(1);
 
         DBCursor c = users.find();
         while (c.hasNext()){
             DBObject d = c.next();
             System.out.println(d);
         }
+
         System.out.println("ok");
     }
 }
