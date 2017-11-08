c472a0f7c9ea7d60424e58ce61691771661b5d37
zhongyiares <zhongyiares@qq.com>
2017-11-08 16:27:28 +0800

    add stockPolicy

---
+++ src/main/webapp/stock/stu/cellphone/js/H5CompBar.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Administrator on 2017/11/6 0006.
+ */
+
+var H5ComponentBar = function ( name,cfg) {
+    var component = new H5ComponentBase( name ,cfg);
+
+    $.each( cfg.data,function (idx ,item) {
+        var line =  $('<div class="line">');
+        var name =  $('<div class="name">');
+        var rate =  $('<div class="rate">');
+        var per =  $('<div class="per">');
+
+        var width = item[1]*100 + '%';
+        var bgStyle = "";
+        if( item[2]){
+            bgStyle = 'style = "background-color:'+item[2]+'" ';
+        }
+
+        rate.html("<div class='bg' "+bgStyle+"></div>");
+        rate.css('width',width);
+
+
+        name.text( item[0]);
+        per.text(width)
+        line.append( name).append( rate ).append(per);
+
+
+        component.append(line);
+    })
+
+    return component;
+}
