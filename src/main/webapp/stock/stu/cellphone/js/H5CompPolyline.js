b7a7748948769f5cd6e71f5695859cd58f1f83f4
zhongyiares <zhongyiares@qq.com>
2017-11-08 18:24:31 +0800

    add stockPolicy

---
+++ src/main/webapp/stock/stu/cellphone/js/H5CompPolyline.js
@@ -0,0 +1,84 @@
+/**
+ * Created by Administrator on 2017/11/6 0006.
+ */
+
+var H5ComponentPolyline = function ( name,cfg) {
+    var component = new H5ComponentBase( name ,cfg);
+
+    //
+    var w = cfg.width;
+    var h = cfg.height;
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width = w;
+    cns.height = ctx.height = h;
+
+    component.append(cns);
+
+    var step = 10;
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#F70";
+
+    window.ctx = ctx;
+    for(var i=0; i< step + 1 ;i++){
+        var y = (h / step) * i;
+        ctx.moveTo(0,y);
+        ctx.lineTo(w,y);
+    }
+
+    step = cfg.data.length + 1;
+    for(var i=0 ; i< step + 1;i++){
+        var x = (w / step) * i;
+        ctx.moveTo(x,0);
+        ctx.lineTo(x,h);
+    }
+    ctx.stroke();
+
+
+
+    //
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width = w;
+    cns.height = ctx.height = h;
+    component.append(cns);
+
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#ff8878";
+
+    var x=0;
+    var y=0;
+    // ctx.moveTo(10,10);
+    // ctx.arc(10,10,5,0,2*Math.PI);
+    // step = cfg.data.length + 1;
+    var row_w = w / (cfg.data.length + 1);
+    for( i in cfg.data){
+        var item = cfg.data[i];
+        x = row_w * i + row_w;
+        y = h * (1- item[1]);
+        ctx.moveTo(x,y)
+        ctx.arc(x,y,5,0,2*Math.PI);
+    }
+
+    ctx.moveTo(row_w,h * (1- cfg.data[0][1]));
+    for(i in cfg.data){
+        var item = cfg.data[i];
+        x = row_w * i + row_w;
+        y = h * (1- item[1]);
+        ctx.lineTo(x,y);
+    }
+    //
+    for( i in cfg.data){
+        var item = cfg.data[i];
+        x = row_w * i + row_w;
+        y = h * (1- item[1]);
+        ctx.fillStyle = item[2] ? item[2] : '#595959'
+        ctx.fillText(((item[1]*100)>>0)+'%',x-10,y-10)
+    }
+
+    ctx.stroke();
+
+    return component;
+}
